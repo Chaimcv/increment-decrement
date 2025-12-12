26b0a959a70489c265c1124e08c8746c37fb3f0d
Chaimcv <chaithanyamcv@gmail.com>
2025-12-12 20:05:06 +0530

    Edits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React,{useState} from "react";
 import './App.css';
 
+
 function App() {
+  const[colour,setColour]=useState(0);
+  const[count,setCount]=useState(0);
+  const increment=function(){
+   setCount(count+5);
+   console.log(count);
+   setColour(count>20?(< span style={{color:"green"}}>{count}</span>):{count});
+  }
+  const decrement=function(){
+    count>0?setCount(count-5):setCount(0);
+  }
+  const result=function(){
+    setCount(0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{count}</h2>
+      <button onClick={increment}>+</button>
+       <button onClick={result}>Reset</button>
+       <button onClick={decrement}>-</button>
     </div>
   );
 }
