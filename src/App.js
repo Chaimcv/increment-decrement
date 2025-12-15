3f5a4309d66d290f47f63843483417a9868fb526
Chaimcv <chaithanyamcv@gmail.com>
2025-12-15 12:35:07 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React,{useState} from "react";
 import './App.css';
 
-
 function App() {
-  const[colour,setColour]=useState(0);
+  
   const[count,setCount]=useState(0);
+  const[colour,setColour]=useState('red');
   const increment=function(){
    setCount(count+5);
-   console.log(count);
-   setColour(count>20?(< span style={{color:"green"}}>{count}</span>):{count});
+   count>15 && count<40?setColour('green'):setColour('blue');
   }
   const decrement=function(){
     count>0?setCount(count-5):setCount(0);
+    count>0 && count<20?setColour('red'):setColour(
+   count>15 && count<40?setColour('green'):setColour('blue'));
   }
   const result=function(){
     setCount(0);
+    setColour('red'); 
   }
-
   return (
     <div className="App">
-      <h2>{count}</h2>
+      {/* <h2>{count}</h2> */}
+      <h2 style={{color:colour}}>{count}</h2>
       <button onClick={increment}>+</button>
        <button onClick={result}>Reset</button>
        <button onClick={decrement}>-</button>
